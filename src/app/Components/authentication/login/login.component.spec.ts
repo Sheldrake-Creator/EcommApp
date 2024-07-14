0af4498faf6db964669c7df7c622605744310d3b
silas.pacheco <silasfpacheco@gmail.com>
2024-07-13 22:42:56 -0400

    fixed login component

--- src/app/Components/authentication/login/login.component.spec.ts
+++ src/app/Components/authentication/login/login.component.spec.ts
@@ -1,18 +1,17 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { SigninComponent } from './login.component';
+import { LoginComponent } from './login.component';
 
 describe('SigninComponent', () => {
-  let component: SigninComponent;
-  let fixture: ComponentFixture<SigninComponent>;
+  let component: LoginComponent;
+  let fixture: ComponentFixture<LoginComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [SigninComponent]
-    })
-    .compileComponents();
-    
-    fixture = TestBed.createComponent(SigninComponent);
+      imports: [LoginComponent],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(LoginComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
