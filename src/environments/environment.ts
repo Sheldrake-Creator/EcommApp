de4db3056576bc3126d5aca021f534fe42b49228
silas.pacheco <silasfpacheco@gmail.com>
2024-08-14 21:05:29 -0400

    Added Https

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  API_URL: 'http://ec2-107-23-24-98.compute-1.amazonaws.com:4545',
+  API_URL: 'https://ec2-107-23-24-98.compute-1.amazonaws.com:4545',
 };
